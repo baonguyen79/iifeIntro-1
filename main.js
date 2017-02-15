3c204f290237e332d22bd13a9aaf10b9cd410f95
Zoe Ames <zoeames@gmail.com>
2017-02-14 19:47:31 -0600

    finished main.js

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
-console.log("work")
+var outside = "global";
+
+function scopeStuff(){
+	var inside = "local";
+	// console.log("outside from inside the function", outside); //cat
+	// console.log("inside from inside the function", inside);
+}
+
+// console.log("outside from outside the function", outside);
+// console.log("inside from outside the function", inside);
+
+// scopeStuff();
+
+var global_base = 500;
+!function throwAway(){
+	global_base+=500;
+	console.log(global_base);//1000
+}();
+
+var cats = (function(){
+	var color = "black";
+	var type = "tiger";
+	var name = "fluffy";
+
+
+
+	return {
+		getColor : function(){
+			var result = "your cat is " + color;
+			return result;
+		},
+		getType : function(){
+			return type;
+		},
+		setColor: function(newColor){
+			color = newColor;
+		}
+	};
+})();
+
+console.log(cats);//
+cats.setColor("pink");
+console.log(cats.getColor());
+
+
+
+// throwAway();
+
+// document.getElementById('cat').addEventListener("click", throwAway)
+
+
+
+
+
+
+
+
+
+
+
+
