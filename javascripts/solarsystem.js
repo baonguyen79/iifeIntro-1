0780816c1c01cb14add7546f0f1fcf308b1c65db
Zoe Ames <zoeames@gmail.com>
2017-02-18 11:13:32 -0600

    added all augmentors

--- javascripts/solarsystem.js
+++ javascripts/solarsystem.js
@@ -1,18 +1,7 @@
 var SolarSystem = (function(){
-	var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
 	var dwarfPlanets = ["pluto"];
-	var numPlanetsPeopleLandedOn = 0;
 
 	return {
-		getPlanets: function(){
-			return planets;
-		},
-		getPlanetsLandedPeopleOn: function(){
-			return numPlanetsPeopleLandedOn;
-		},
-		setPlanetsLandedPeopleOn: function(){
-			numPlanetsPeopleLandedOn++;
-		},
 		getDwarfPlanets : function(){
 			return "they are rejects";
 		},
@@ -21,20 +10,3 @@ var SolarSystem = (function(){
 		}
 	}
 })();
-
-
-// console.log("Planets: ", SolarSystem.getPlanets());
-// console.log("Planets w/ people: ", SolarSystem.getPlanetsLandedPeopleOn());
-// SolarSystem.setSpaceships("Voyager 1");
-// SolarSystem.setSpaceships("Voyager 2");
-// SolarSystem.setSpaceships("Gemini");
-// SolarSystem.setSpaceships("Apollo");
-// console.log("Spaceships: ", SolarSystem.getSpaceships());
-
-
-
-
-
-
-
-
